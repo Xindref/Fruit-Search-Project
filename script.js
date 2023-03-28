e89a9ba6a5d7b8a1bef28344bc1f53aba75ab0f2
Xindref <dakotahmccrary@gmail.com>
2023-03-28 03:39:07 -0500

    added a regex for the strong tag, and moved the hover logic to the ul

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 // searches the fruit array for any fruit containing the value of str,
 // returns a filtered version of the fruit array in results array
 const search = (str) => {
-	let results = [];
 	if (str !== '') {
-		results = fruit.filter((f) => f.toLowerCase().includes(str.toLowerCase()));
+		let results = fruit.filter((f) => f.toLowerCase().includes(str.toLowerCase()));
 		showSuggestions(results, str);
 		return results;
 	}
@@ -25,19 +24,24 @@ const searchHandler = () => search(input.value);
 // formats the bold patterns for fruits containing inputVal
 const insertBold = (str, indexStart, indexEnd) => `${str.substring(0, indexStart)}<b>${str.substring(indexStart, indexEnd)}</b>${str.substring(indexEnd)}`
 
+// second version of formatting using a regular expression and strong tag
+const insertBoldV2 = (str, textToBold) => {
+	const regex = new RegExp(textToBold, 'gi');
+	return str.replace(regex, (match) => `<strong>${match}</strong>`)
+}
+
 const showSuggestions = (results, inputVal) => {
 	// empties suggestions innerHTML to make sure it updates properly
 	suggestions.innerHTML = '';
 	// loops through results array to create li for each match
 	results.forEach(result => {
 		newli = document.createElement('li');
+		// set class for the LI so we can reference it for hover
 		newli.classList.add('searchSuggestion');
-		// event listener for mouseleave to handle color and pointer changes
-		newli.addEventListener('mouseleave', unHoverSuggestion);
 		// setting initial index of where inputVal is found in fruit
 		let index = result.toLowerCase().indexOf(inputVal.toLowerCase());
 		// calls insertBold to make the input chars fruit includes to be bold
-		newli.innerHTML = insertBold(result, index, (index + inputVal.length));
+		newli.innerHTML = insertBoldV2(result, inputVal);
 		suggestions.append(newli);
 	});
 }
@@ -45,13 +49,14 @@ const showSuggestions = (results, inputVal) => {
 // empties suggestion list and sets suggestion to input value
 const useSuggestion = (e) => {
 	e.preventDefault();
-	// because the eventlistener will pick up the bold tag, this is needed to
+	// because the eventlistener will pick up the strong tag, this is needed to
 	// handle the suggestion for the parent li element
-	if (e.target.tagName === 'B') {
+	if (e.target.parentElement.className === 'searchSuggestion') {
 		input.value = e.target.parentElement.innerText;
 		search(e.target.parentElement.innerText);
 	} else {
-		// if not bold, then it will use the innerText of the li element
+		// if not clicking the strong tag, then it will use the innerText of the
+		// li element
 		input.value = e.target.innerText;
 		search(e.target.innerText);
 	}
@@ -61,27 +66,27 @@ const useSuggestion = (e) => {
 // handles color and pointer changes on hover
 const hoverSuggestion = (e) => {
 	e.preventDefault();
-	// like the previous function, if you hover over the bold portion it has to be
-	// handled seperately. Sets the parentElement background color
-	if (e.target.tagName === 'B') {
-		e.target.parentElement.style.background = hoverColor;
-	}
-	// if not hovering the bold portion, we can just operate off the li tag itself
-	else if (e.target.tagName === 'LI') {
+	if (e.target.className === 'searchSuggestion') {
 		e.target.style.background = hoverColor;
+	}
+	else if (e.target.parentElement.className === 'searchSuggestion') {
+		e.target.parentElement.style.background = hoverColor;
 	}
 	document.body.style.cursor = "pointer";
 }
 
-// changes cursor back to default, and color back to previous color before hover
-// because we don't want anything to change if you leave a bold character in 
-// the li, we do not need to check for bold tag here
+// changes cursor back to default, and color back to previous color.
+// Because we don't want anything to change if you unhover a strong character in 
+// the li, we do not need to check for the strong tag here
 const unHoverSuggestion = (e) => {
 	e.preventDefault();
-	document.body.style.cursor = 'default';
-	e.target.style.background = 'rgba(255, 215, 140, 0.397)';
+	if (e.target.className === 'searchSuggestion') {
+		document.body.style.cursor = 'default';
+		e.target.style.background = 'rgba(255, 215, 140, 0.397)';
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
 suggestions.addEventListener('mouseover', hoverSuggestion);
+suggestions.addEventListener('mouseout', unHoverSuggestion);
