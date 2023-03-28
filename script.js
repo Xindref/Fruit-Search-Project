c3fbe863cbf51284e480d7a59318f50e1366df9d
Xindref <dakotahmccrary@gmail.com>
2023-03-28 13:10:56 -0500

    Added some css to handle a class change instead of direct styles, and changed some function and variable names

--- script.js
+++ script.js
@@ -1,92 +1,92 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
-// Set hoverColor here, so that it can be easily changed.
-const hoverColor = 'orangered';
+// Set highlight and search class here, so that it can be easily changed.
+const HIGHLIGHT_CLASS = 'highlighted-suggestion';
+const SEARCH_SUGGESTION = 'search-suggestion';
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 // searches the fruit array for any fruit containing the value of str,
 // returns a filtered version of the fruit array in results array
-const search = (str) => {
-	if (str !== '') {
-		let results = fruit.filter((f) => f.toLowerCase().includes(str.toLowerCase()));
-		showSuggestions(results, str);
-		return results;
+const search = (input) => {
+	if (input !== '') {
+		let searchResults = fruits.filter((fruit) => {
+			const inputRegEx = new RegExp(input, 'i');
+			return fruit.match(inputRegEx);
+		});
+		displaySuggestions(searchResults, input);
+		return searchResults;
 	}
 	// set suggestions back to empty when str is empty
 	suggestions.innerHTML = '';
 }
 
-// get string from input field
-const searchHandler = () => search(input.value);
+// get string from input field and call search on it
+const handleSearch = () => search(input.value);
 
-// formats the bold patterns for fruits containing inputVal
-const insertBold = (str, indexStart, indexEnd) => `${str.substring(0, indexStart)}<b>${str.substring(indexStart, indexEnd)}</b>${str.substring(indexEnd)}`
-
-// second version of formatting using a regular expression and strong tag
-const insertBoldV2 = (str, textToBold) => {
-	const regex = new RegExp(textToBold, 'gi');
+// formats a string using regular expression to make matching portion strong tagged
+const insertBold = (str, textToBold) => {
+	const regex = new RegExp(textToBold, 'i');
 	return str.replace(regex, (match) => `<strong>${match}</strong>`)
 }
 
-const showSuggestions = (results, inputVal) => {
+const displaySuggestions = (searchResults, inputVal) => {
 	// empties suggestions innerHTML to make sure it updates properly
 	suggestions.innerHTML = '';
-	// loops through results array to create li for each match
-	results.forEach(result => {
-		newli = document.createElement('li');
-		// set class for the LI so we can reference it for hover
-		newli.classList.add('searchSuggestion');
-		// setting initial index of where inputVal is found in fruit
-		let index = result.toLowerCase().indexOf(inputVal.toLowerCase());
+	// loops through search results and creates an li for each
+	searchResults.forEach(fruit => {
+		const newListItem = document.createElement('li');
+		// set class for the LI so we can reference it in our highlight
+		newListItem.classList.add(SEARCH_SUGGESTION);
 		// calls insertBold to make the input chars fruit includes to be bold
-		newli.innerHTML = insertBoldV2(result, inputVal);
-		suggestions.append(newli);
+		newListItem.innerHTML = insertBold(fruit, inputVal);
+		// appends the newListItem to the suggestions element
+		suggestions.append(newListItem);
 	});
 }
 
 // empties suggestion list and sets suggestion to input value
-const useSuggestion = (e) => {
-	e.preventDefault();
+const useSuggestion = (event) => {
+	event.preventDefault();
 	// because the eventlistener will pick up the strong tag, this is needed to
 	// handle the suggestion for the parent li element
-	if (e.target.parentElement.className === 'searchSuggestion') {
-		input.value = e.target.parentElement.innerText;
-		search(e.target.parentElement.innerText);
+	if (event.target.parentElement.classList.contains(SEARCH_SUGGESTION)) {
+		input.value = event.target.parentElement.innerText;
 	} else {
 		// if not clicking the strong tag, then it will use the innerText of the
 		// li element
-		input.value = e.target.innerText;
-		search(e.target.innerText);
+		input.value = event.target.innerText;
 	}
+	// clear suggestions once we select an item from suggestions
 	suggestions.innerHTML = '';
 }
 
-// handles color and pointer changes on hover
-const hoverSuggestion = (e) => {
-	e.preventDefault();
-	if (e.target.className === 'searchSuggestion') {
-		e.target.style.background = hoverColor;
+// handles color and pointer changes on mouse enter
+const highlightSuggestion = (event) => {
+	event.preventDefault();
+	// checks class name, and adds the highlight class
+	if (event.target.className === SEARCH_SUGGESTION) {
+		event.target.classList.add(HIGHLIGHT_CLASS);
 	}
-	else if (e.target.parentElement.className === 'searchSuggestion') {
-		e.target.parentElement.style.background = hoverColor;
+	// if it's the strong tag, then we need to get/set the parent elements class
+	else if (event.target.parentElement.className === SEARCH_SUGGESTION) {
+		event.target.parentElement.classList.add(HIGHLIGHT_CLASS);
 	}
-	document.body.style.cursor = "pointer";
 }
 
-// changes cursor back to default, and color back to previous color.
 // Because we don't want anything to change if you unhover a strong character in 
 // the li, we do not need to check for the strong tag here
-const unHoverSuggestion = (e) => {
-	e.preventDefault();
-	if (e.target.className === 'searchSuggestion') {
-		document.body.style.cursor = 'default';
-		e.target.style.background = 'rgba(255, 215, 140, 0.397)';
+const unHighlightSuggestion = (event) => {
+	event.preventDefault();
+	// checks if classList has search-suggestion
+	if (event.target.classList.contains(SEARCH_SUGGESTION)) {
+		// removes the highlight class on mouse out
+		event.target.classList.remove(HIGHLIGHT_CLASS);
 	}
 }
 
-input.addEventListener('keyup', searchHandler);
+input.addEventListener('keyup', handleSearch);
 suggestions.addEventListener('click', useSuggestion);
-suggestions.addEventListener('mouseover', hoverSuggestion);
-suggestions.addEventListener('mouseout', unHoverSuggestion);
+suggestions.addEventListener('mouseover', highlightSuggestion);
+suggestions.addEventListener('mouseout', unHighlightSuggestion);
